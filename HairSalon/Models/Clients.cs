c059c628adfea91e7804a6a8604727fe310d795b
Jimmy Zebroski <zebrozkii@gmail.com>
2019-03-10 17:46:55 -0700

    might have found a problem with my database

--- HairSalon/Models/Clients.cs
+++ HairSalon/Models/Clients.cs
@@ -41,8 +41,8 @@ namespace HairSalon.Models
         {
         int clientId = rdr.GetInt32(0);
         string clientName = rdr.GetString(1);
-        int clientStylistID = rdr.GetInt32(2);
-        Client newClient = new Client(clientName, clientId, clientStylistID);
+        int clientStylistId = rdr.GetInt32(2);
+        Client newClient = new Client(clientName, clientId, clientStylistId);
         allClients.Add(newClient);
         }
         if(conn != null)
@@ -157,7 +157,7 @@ namespace HairSalon.Models
           MySqlConnection conn = DB.Connection();
           conn.Open();
           var cmd = conn.CreateCommand() as MySqlCommand;
-          cmd.CommandText = @"DELETE FROM clients WHERE id = @clientId;";
+          cmd.CommandText = @"DELETE FROM clients WHERE id = @clientId;"; //<--- might need a s on that @clientId
           MySqlParameter clientIdParameter = new MySqlParameter();
           clientIdParameter.ParameterName = "@clientId";
           clientIdParameter.Value = this.GetId();

1f6369568fc0f3e9013668fbb37ac2b791445fc4
Jimmy Zebroski <zebrozkii@gmail.com>
2019-03-16 11:00:58 -0700

    my controllers keep breaking still fixing a few things

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -13,12 +13,6 @@ namespace HairSalon.Controllers
       List<Client> allClients = Client.GetAll();
       return View(allClients);
     }
-
-        [HttpGet("/clients/new")]
-        public ActionResult New()
-        {
-          return View();
-        }
       [HttpGet("/clients/deleteall")]
       public ActionResult DeleteAll()
       {
@@ -30,14 +24,13 @@ namespace HairSalon.Controllers
         public ActionResult New(int stylistId)
         {
             Stylist stylist = Stylist.Find(stylistId);
-
             return View(stylist);
         }
         [HttpGet("/clients/{clientId}")]
         public ActionResult Show(int clientId)
         {
           Dictionary<string, object> model = new Dictionary<string, object>();
-            Client newClient = Client.Find(clientId);
+          Client newClient = Client.Find(clientId);
           Stylist newStylist = Stylist.Find(newClient.GetStylistId());
           model.Add("client",newClient);
           model.Add("stylist",newStylist);
